d5af52fe44025859eadf7ac43927a80ab221baf0
Lu3kyy <fabledparadox@gmail.com>
2026-03-27 09:21:52 -0700

    1234

--- obj/Release/net9.0/BlogApiPrev.AssemblyInfo.cs
+++ obj/Release/net9.0/BlogApiPrev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApiPrev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23cb068b8a23a86f4cccfc96daf8cf948e6ec760")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApiPrev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApiPrev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
